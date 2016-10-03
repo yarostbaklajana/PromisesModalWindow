b33d96a6a258ba5b8a1f661af086cadec376fea8
yarostbaklajana <yarostbaklajana@gmail.com>
2016-10-03 13:05:54 +0300

    added tslint configuration for checking files for mistakes

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,8 @@
  * Created by yaros on 02.10.2016.
  */
 
-
 module.exports = {
-    
+
     entry: {
         filename: './app/main.ts'
     },
@@ -18,7 +17,6 @@ module.exports = {
         extensions: ['', '.ts']
     },
 
-    // Add loader for .ts files.
     module: {
         loaders: [
             {
@@ -26,6 +24,36 @@ module.exports = {
                 loader: 'awesome-typescript-loader',
                 exclude: /node_modules/
             }
-        ]
+        ],
+
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                loader: "tslint"
+            }
+        ],
+
+    },
+
+    tslint: {
+        configuration: {
+            rules: {
+                "indent": [true, "spaces"]
+            }
+        },
+
+        emitErrors: false,
+
+        failOnHint: false,
+
+        fileOutput: {
+            dir: "./tsLint_report",
+            ext: "xml",
+            clean: true,
+            header: "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle version=\"5.7\">",
+            footer: "</checkstyle>"
+        }
     }
-};
+}
+
+
